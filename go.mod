c4c95a76122245fb85efef801b5c9180408eab8b
Edmund Debrah <edmundking05@gmail.com>
2023-09-14 13:53:21 +0000

    set up api

--- go.mod
+++ go.mod
@@ -2,17 +2,18 @@ module github.com/codewithed/hng_ix_02
 
 go 1.21.0
 
-require github.com/gofiber/fiber v1.14.6
+require github.com/gofiber/fiber/v2 v2.49.1
 
 require (
-	github.com/andybalholm/brotli v1.0.0 // indirect
-	github.com/gofiber/utils v0.0.10 // indirect
-	github.com/gorilla/schema v1.1.0 // indirect
-	github.com/klauspost/compress v1.10.7 // indirect
-	github.com/mattn/go-colorable v0.1.7 // indirect
-	github.com/mattn/go-isatty v0.0.12 // indirect
+	github.com/andybalholm/brotli v1.0.5 // indirect
+	github.com/google/uuid v1.3.1 // indirect
+	github.com/klauspost/compress v1.16.7 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
-	github.com/valyala/fasthttp v1.16.0 // indirect
-	github.com/valyala/tcplisten v0.0.0-20161114210144-ceec8f93295a // indirect
-	golang.org/x/sys v0.0.0-20200602225109-6fdc65e7d980 // indirect
+	github.com/valyala/fasthttp v1.49.0 // indirect
+	github.com/valyala/tcplisten v1.0.0 // indirect
+	golang.org/x/sys v0.11.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,1 +1,126 @@
 package main
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+
+	db "github.com/codewithed/hng_ix_02/db/sqlc"
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	conn := ""
+	sqlDB, err := sql.Open("postgres", conn)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	queries := db.New(sqlDB)
+
+	router := fiber.New()
+
+	router.Post("/", func(c *fiber.Ctx) error {
+		params := db.CreatePersonParams{}
+		err := c.BodyParser(&params)
+		if err != nil {
+			return c.Status(http.StatusBadRequest).JSON(fiber.Map{
+				"error": err.Error(),
+			})
+		}
+
+		user, err := queries.CreatePerson(context.Background(), params)
+		if err != nil {
+			return c.Status(http.StatusInternalServerError).JSON(fiber.Map{
+				"error": err.Error(),
+			})
+		}
+		return c.Status(http.StatusOK).JSON(user)
+	})
+
+	router.Get("/:name", func(c *fiber.Ctx) error {
+		name := c.Params("name")
+		user, err := queries.GetPerson(context.Background(), name)
+		if err != nil {
+			if err == sql.ErrNoRows {
+				return c.Status(http.StatusNotFound).JSON(fiber.Map{
+					"error": err.Error(),
+				})
+			}
+			return c.Status(http.StatusInternalServerError).JSON(fiber.Map{
+				"error": err.Error(),
+			})
+		}
+
+		return c.Status(http.StatusOK).JSON(user)
+	})
+
+	router.Put("/:name", func(c *fiber.Ctx) error {
+		params := db.UpdatePersonParams{}
+		err := c.BodyParser(&params)
+		if err != nil {
+			return c.Status(http.StatusBadRequest).JSON(fiber.Map{
+				"error": err.Error(),
+			})
+		}
+
+		user, err := queries.UpdatePerson(context.Background(), params)
+		if err != nil {
+			if err == sql.ErrNoRows {
+				return c.Status(http.StatusNotFound).JSON(fiber.Map{
+					"error": err.Error(),
+				})
+			}
+			return c.Status(http.StatusInternalServerError).JSON(fiber.Map{
+				"error": err.Error(),
+			})
+		}
+		return c.Status(http.StatusOK).JSON(user)
+	})
+
+	router.Patch("/:name", func(c *fiber.Ctx) error {
+		params := db.UpdatePersonParams{}
+		err := c.BodyParser(&params)
+		if err != nil {
+			return c.Status(http.StatusBadRequest).JSON(fiber.Map{
+				"error": err.Error(),
+			})
+		}
+
+		user, err := queries.UpdatePerson(context.Background(), params)
+		if err != nil {
+			if err == sql.ErrNoRows {
+				return c.Status(http.StatusNotFound).JSON(fiber.Map{
+					"error": err.Error(),
+				})
+			}
+			return c.Status(http.StatusInternalServerError).JSON(fiber.Map{
+				"error": err.Error(),
+			})
+		}
+		return c.Status(http.StatusOK).JSON(user)
+	})
+
+	router.Delete("/:name", func(c *fiber.Ctx) error {
+		name := c.Params("name")
+		err := queries.DeletePerson(context.Background(), name)
+		if err != nil {
+			if err == sql.ErrNoRows {
+				return c.Status(http.StatusNotFound).JSON(fiber.Map{
+					"error": err.Error(),
+				})
+			}
+			return c.Status(http.StatusInternalServerError).JSON(fiber.Map{
+				"error": err.Error(),
+			})
+		}
+
+		msg := fmt.Sprintf("deleted person: %v", name)
+		return c.Status(http.StatusOK).JSON(fiber.Map{
+			"message": msg,
+		})
+	})
+
+}
